c61f3105aeb37c86a2a3009a4637dfc7208048d1
Diego Gutemberg Barbosa <64646533+diegobarbosa86@users.noreply.github.com>
2021-10-23 15:02:08 -0300

    update

--- selecionar-o-dom/js/ex.js
+++ selecionar-o-dom/js/ex.js
@@ -50,7 +50,8 @@ window.onload = function () {
 
 const links = document.querySelectorAll("a"); //selecionar todos os links
 
-links.forEach((link) => {//para cada item de 'links'
+links.forEach((link) => {
+  //para cada item de 'links'
   const linkWidth = link.offsetWidth;
   const linkHeight = link.offsetHeight;
   if (linkWidth >= 48 && linkHeight >= 48) {
@@ -63,8 +64,66 @@ links.forEach((link) => {//para cada item de 'links'
 // Se o browser for menor que 720px,
 // adicione a classe menu-mobile ao menu
 
-const browserSmall = window.matchMedia('(max-width:720px').matches
-if(browserSmall){
-  const menu = document.querySelector('.menu')
-  menu.classList.add('menu-mobile')
+const browserSmall = window.matchMedia("(max-width:720px").matches;
+if (browserSmall) {
+  const menu = document.querySelector(".menu");
+  menu.classList.add("menu-mobile");
 }
+
+//eventos
+
+const img = document.querySelector("img");
+
+function callback() {
+  console.log("clicou");
+}
+
+img.addEventListener("click", callback); //separar a função de callback do addEventListener
+
+// Quando o usuário clicar nos links internos do site,
+// adicione a classe ativo ao item clicado e remova dos
+// demais itens caso eles possuam a mesma. Previna
+// o comportamento padrão desses links
+
+const links2 = document.querySelectorAll('a[href^="#"]');
+
+function handleLink(event) {
+  event.preventDefault();
+  links2.forEach((link) => {
+    link.classList.remove("ativo");
+  });
+  event.currentTarget.classList.add("ativo");
+}
+
+links2.forEach((link) => {
+  link.addEventListener("click", handleLink);
+});
+
+// Selecione todos os elementos do site começando a partir do body,
+// ao clique mostre exatamente quais elementos estão sendo clicados
+const todosElementos = document.querySelectorAll("body *");
+
+function handleElemento(event){
+  console.log(event.currentTarget)
+}
+
+
+//function handleElemento(event) {
+//  event.currentTarget.remove();
+//}
+
+todosElementos.forEach((elemento) => {
+  elemento.addEventListener("click", handleElemento);
+});
+// Utilizando o código anterior, ao invés de mostrar no console,
+// remova o elemento que está sendo clicado, o método remove() remove um elemento
+
+// Se o usuário clicar na tecla (t), aumente todo o texto do site.
+
+function handleClickT(event) {
+  console.log(event.key);
+  if(event.key === 't')
+    document.documentElement.classList.toggle('textomaior')//documentElement seleciona o HTML
+}
+
+window.addEventListener('keydown', handleClickT)
